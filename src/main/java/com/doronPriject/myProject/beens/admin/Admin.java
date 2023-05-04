86c3849d78f9bae5e1de6dedf17ccd83e0746a89
Doron <doronelijah@gmail.com>
2023-05-04 23:33:29 +0300

    all good

---
+++ src/main/java/com/doronPriject/myProject/beens/admin/Admin.java
@@ -0,0 +1,18 @@
+package com.doronPriject.myProject.beens.admin;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import lombok.*;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Admin {
+
+    private String email = "admin";
+
+    private String password = "admin";
+
+
+
+}
